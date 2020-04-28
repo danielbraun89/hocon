26eb1782c4af0241d8cd065604aca653bc2c39eb
Daniel Braun <braundaniel@live.com>
2020-04-28 15:46:34 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# HOCON
+
+HOCON, or Human-Optimized Config Object Notation is a format for human-readable data, and a superset of JSON.
+
+
+`hocon` exposes an API familiar to users of the standard library `json`, `marshal` and `pickle` modules.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hocon",
+    version="0.0.1",
+    author="Daniel Braun",
+    author_email="danie.braun@outlook.co.il",
+    description="HOCON encoder and decoder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danielbraun89/hocon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
