d19ab9765abf3cca9ef740eddcb1b5303fd4da19
Daniel Braun <daniel.braun@outlook.co.il>
2020-04-29 18:29:57 +0300

    support for json compatible hocon files

--- hocon/__init__.py
+++ hocon/__init__.py
@@ -1,1 +1,22 @@
-name = "example_pkg"
+from typing import Any, TextIO, AnyStr, Dict
+from pyhocon import ConfigFactory, HOCONConverter
+import json
+
+__version__ = "0.0.1"
+
+
+def dump(obj: Any, fp: TextIO):
+    json.dump(dumps(obj), fp)
+
+
+def dumps(obj: Any) -> str:
+    return HOCONConverter.to_json(obj)
+
+
+def load(fp: TextIO) -> Dict[str, Any]:
+    return loads(fp.read())
+
+
+def loads(s: AnyStr) -> Dict[str, Any]:
+    hocon_obj = ConfigFactory.parse_string(s)
+    return json.loads(HOCONConverter.to_json(hocon_obj))
