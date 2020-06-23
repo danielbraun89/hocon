c13df2779c087f2649b62f3f87a48d25435cbb55
Daniel Braun <daniel.braun@outlook.co.il>
2020-06-23 11:59:32 +0300

    better handling of relative includes when loading from string

--- hocon/decoder.py
+++ hocon/decoder.py
@@ -1,13 +1,14 @@
 import json
+import os
 from typing import Any, TextIO, AnyStr, Dict
 
 from pyhocon import ConfigFactory, HOCONConverter
 
 
 def load(fp: TextIO) -> Dict[str, Any]:
-    return loads(fp.read())
+    return loads(fp.read(), basedir=os.path.dirname(fp.name))
 
 
-def loads(s: AnyStr) -> Dict[str, Any]:
-    hocon_obj = ConfigFactory.parse_string(s)
+def loads(s: AnyStr, **kwargs: Any) -> Dict[str, Any]:
+    hocon_obj = ConfigFactory.parse_string(s, **kwargs)
     return json.loads(HOCONConverter.to_json(hocon_obj))
